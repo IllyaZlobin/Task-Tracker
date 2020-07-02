245be302a159e04625b23c15bb118b35a1cccc6b
illiaZlobin <illiazlobin@gmail.com>
2020-07-02 13:54:05 +0300

    changed log output

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(logger("dev"));
+app.use(logger("common"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
